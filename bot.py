5f524b52057521d399cdd126f353591391ab5de9
sinapourmahmoud <sinapoormahmood@gmail.com>
2025-10-17 11:00:29 -0400

    fix some problems and add flask to project

--- bot.py
+++ bot.py
@@ -3,26 +3,34 @@ from handlers.register_hello import register_hello
 from handlers.button_handlers import register_buttons
 import dotenv
 import os
+from flask import Flask
+import threading
+
 dotenv.load_dotenv()
 
-class UniversityBot():
-    
+class UniversityBot:
     def __init__(self):
-        self.bot=telebot.TeleBot(os.getenv('BOT_TOKEN'))
-        self.handlers()
-        
-    def handlers(self):
+        self.bot = telebot.TeleBot(os.getenv('BOT_TOKEN'))
+        self._register_handlers()
+    
+    def _register_handlers(self):
         register_hello(self.bot)
-
         register_buttons(self.bot)
-        
+
+    def run(self):
+        self.bot.polling(non_stop=True)
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "🤖 University Bot is running successfully!"
+
+def run_web():
+    app.run(host='0.0.0.0', port=8080)
+
+if __name__ == "__main__":
+    threading.Thread(target=run_web).start()
     
-    
-    def end(self):
-        self.bot.polling()
-    
-    
-if __name__=="__main__":
-    university=UniversityBot()
-    university.end()
-    
+    university = UniversityBot()
+    university.run()
